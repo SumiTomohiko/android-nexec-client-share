d1093e9fade1fcd012ccfe3aeb7c3e8d8fe38ab7
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2014-03-15 23:59:09 +0900

    Added the interface with the constants.

---
+++ src/jp/gr/java_conf/neko_daisuki/android/nexec/client/share/NexecConstants.java
@@ -0,0 +1,7 @@
+package jp.gr.java_conf.neko_daisuki.android.nexec.client.share;
+
+public interface NexecConstants {
+
+    public static final String EXTRA_HOST = "host";
+    public static final String EXTRA_PORT = "port";
+}
