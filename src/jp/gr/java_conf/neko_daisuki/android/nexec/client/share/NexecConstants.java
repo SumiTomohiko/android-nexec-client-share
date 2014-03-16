3f2ec7fd36cfc94fd003a94d4f6bd078da004800
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2014-03-16 22:51:22 +0900

    Changed the keys to upper case.

--- src/jp/gr/java_conf/neko_daisuki/android/nexec/client/share/NexecConstants.java
+++ src/jp/gr/java_conf/neko_daisuki/android/nexec/client/share/NexecConstants.java
@@ -2,6 +2,6 @@ package jp.gr.java_conf.neko_daisuki.android.nexec.client.share;
 
 public interface NexecConstants {
 
-    public static final String EXTRA_HOST = "host";
-    public static final String EXTRA_PORT = "port";
+    public static final String EXTRA_HOST = "HOST";
+    public static final String EXTRA_PORT = "PORT";
 }
